f41b201ab24f41d941f9ec2a79817900566ed11f
Asi Hassan <asi.hassan111@gmail.com>
2024-04-15 22:23:35 +0300

    fix

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ import { LoginForm } from '../components/LoginForm'
 export default function LoginPage() {
   return (
     <div>
-      <h1>Login Page hello</h1>
+      <h1>Login Page</h1>
       <LoginForm></LoginForm>
     </div>
   )
